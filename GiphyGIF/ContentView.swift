188f5dfad65f64a3b89a0e1250f37ab319953a59
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-05-23 11:41:54 +0800

    initial commit

---
+++ GiphyGIF/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GiphyGIF
+//
+//  Created by Uwais Alqadri on 23/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GiphyGIF/Dependency/AppAssembler.swift
@@ -0,0 +1,8 @@
+//
+//  AppAssembler.swift
+//  GiphyGIF
+//
+//  Created by Uwais Alqadri on 23/05/21.
+//
+
+import Foundation
