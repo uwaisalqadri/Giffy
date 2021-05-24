57c89b548e0018aa510d64c7d5e4835a5632832a
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-05-24 18:08:48 +0800

    inject viewmodel in preview

--- GiphyGIF/App/Features/Home/Views/HomeView.swift
+++ GiphyGIF/App/Features/Home/Views/HomeView.swift
@@ -28,8 +28,9 @@ struct HomeView: View {
   }
 }
 
-//struct HomeView_Previews: PreviewProvider {
-//  static var previews: some View {
-//    HomeView()
-//  }
-//}
+struct HomeView_Previews: PreviewProvider {
+  static var previews: some View {
+    let assembler = AppAssembler()
+    HomeView(viewModel: assembler.resolve())
+  }
+}
