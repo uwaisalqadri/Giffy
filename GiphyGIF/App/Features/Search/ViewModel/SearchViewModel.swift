b4b15a410c7633f7df92c1d7a84d492d7264f6ce
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-05-30 09:32:07 +0700

    remove print

--- GiphyGIF/App/Features/Search/ViewModel/SearchViewModel.swift
+++ GiphyGIF/App/Features/Search/ViewModel/SearchViewModel.swift
@@ -48,7 +48,6 @@ class SearchViewModel: ObservableObject {
         switch completion {
         case .failure:
           self.errorMessage = String(describing: completion)
-          print(self.errorMessage)
         case .finished:
           self.loadingState = false
         }
