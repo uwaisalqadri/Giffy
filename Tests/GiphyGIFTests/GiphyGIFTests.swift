7e3b32c99925e8a9218c8b2316665746679bae83
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2021-11-24 06:41:30 +0700

    update configurations

--- Tests/GiphyGIFTests/GiphyGIFTests.swift
+++ Tests/GiphyGIFTests/GiphyGIFTests.swift
@@ -1,11 +1,11 @@
-    import XCTest
-    @testable import GiphyGIF
+import XCTest
+@testable import GiphyGIF
 
-    final class GiphyGIFTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(GiphyGIF().text, "Hello, World!")
-        }
-    }
+final class GiphyGIFTests: XCTestCase {
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    XCTAssertEqual(GiphyGIF().text, "Hello, World!")
+  }
+}
