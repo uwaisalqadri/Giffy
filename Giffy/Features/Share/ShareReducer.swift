27013765f8f2e7c268b1dda03017165a0b6d5dc2
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2024-12-29 07:54:33 +0700

    feat: share implementation for outside apps

--- Giffy/Features/Share/ShareReducer.swift
+++ Giffy/Features/Share/ShareReducer.swift
@@ -10,9 +10,12 @@ import Common
 import CommonUI
 import Foundation
 import SwiftUI
+import Social
+import MessageUI
+import Photos
 
 @Reducer
-public struct ShareReducer {
+public class ShareReducer {
   
   @Route var router
   
@@ -20,6 +23,20 @@ public struct ShareReducer {
   public struct State: Equatable {
     var topItems: [ShareType] = [.whatsapp, .instagram, .twitter, .telegram]
     var bottomItems: [ShareType] = [.copy, .more]
+    var showShareSheet: Bool = false
+    var shareImage: UIImage {
+      if let image = image, let cachedImage = UIImage(data: image) {
+        return cachedImage
+      }
+      return UIImage()
+    }
+    var shareCaption: String {
+      "Product Caption"
+    }
+    var appName: String? {
+      Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String
+    }
+    var utmFormat: String = "?utm_source=%@&utm_medium=social&utm_campaign=product_share"
     
     var image: Data?
     init(_ image: Data?) {
@@ -35,22 +52,85 @@ public struct ShareReducer {
     Reduce { state, action in
       switch action {
       case let .onShare(type):
+        let imageUtmShare = String(format: state.utmFormat, type.rawValue)
+
         switch type {
         case .whatsapp:
-          break
+          let escapedString = state.shareCaption.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) ?? ""
+          guard let actionUrl = URL(string: "https://wa.me/?text=\(escapedString)\(imageUtmShare)") else { return .none }
+          self.shareTo(url: actionUrl, with: ShareType.whatsapp.rawValue)
+          
         case .instagram:
-          break
+          self.postImageToInstagram(state.shareImage, appName: state.appName ?? "-")
+          
         case .twitter:
-          break
+          let escapedString = state.shareCaption.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) ?? ""
+          guard let appUrl = URL(string: "twitter://post?message=\(escapedString)\(imageUtmShare)") else { return .none }
+          
+          if UIApplication.shared.canOpenURL(appUrl) {
+            self.shareTo(url: appUrl, with: ShareType.twitter.rawValue)
+          } else {
+            guard let webUrl = URL(string: "https://twitter.com/intent/tweet?text=\(escapedString)\(imageUtmShare)") else { return .none }
+            UIApplication.shared.open(webUrl)
+          }
+          
         case .telegram:
-          break
+          let escapedString = state.shareCaption.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) ?? ""
+          guard let url = URL(string: "tg://msg?text=\(escapedString)\(imageUtmShare)") else { return .none }
+          self.shareTo(url: url, with: ShareType.telegram.rawValue)
+          
         case .copy:
           Toaster.success(message: Localizable.labelCopied.tr()).show()
+          
         case .more:
-          break
+          state.showShareSheet.toggle()
+          
         }
         return .none
       }
+    }
+  }
+  
+  func postImageToInstagram(_ image: UIImage, appName: String) {
+    if UIApplication.shared.canOpenURL(URL(string: "instagram://app")!) {
+      PHPhotoLibrary.requestAuthorization { [weak self] status in
+        guard let self = self else { return }
+        
+        if status == .authorized {
+          UIImageWriteToSavedPhotosAlbum(image, self, #selector(self.image(_:didFinishSavingWithError:contextInfo:)), nil)
+        } else {
+          Toaster.error(message: "\(appName) membutuhkan izin anda untuk menulis file ke Photo Library").show()
+        }
+      }
+    } else {
+      Toaster.error(message: "Instagram (X app) tidak terinstal").show()
+    }
+  }
+  
+  @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+    if error != nil {
+      Toaster.error(message: "Gagal menyimpan gambar, cek storage iphone kamu.").show()
+      return
+    }
+    let fetchOptions = PHFetchOptions()
+    fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+    fetchOptions.fetchLimit = 1
+    let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+    if let lastAsset = fetchResult.firstObject {
+      let url = URL(string: "instagram://library?LocalIdentifier=\(lastAsset.localIdentifier)")!
+      if UIApplication.shared.canOpenURL(url) {
+        UIApplication.shared.open(url)
+      } else {
+        Toaster.error(message: "Instagram (X app) tidak terinstal").show()
+      }
+    }
+  }
+  
+  private func shareTo(url: URL, with name: String) {
+    if UIApplication.shared.canOpenURL(url) {
+      UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    } else {
+      Toaster.error(message: "\(name) tidak terinstal.").show()
     }
   }
 }
@@ -68,7 +148,7 @@ public extension ShareReducer {
       UUID().uuidString
     }
     
-    var imageName: String {
+    var rawValue: String {
       switch self {
       case .whatsapp:
         return "whatsapp"
